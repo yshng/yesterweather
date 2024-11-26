17832831cc24d5e9bbb4e8e70b6bc139c5c2b361
yshng <107883486+yshng@users.noreply.github.com>
2024-11-26 10:33:51 -0600

    update unitcontext object to include singular and plural forms

--- src/components/UnitContext.tsx
+++ src/components/UnitContext.tsx
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const UnitContext = createContext({temp: '', depth: ''});
+export const UnitContext = createContext({temp: '', depth: {unit: '', plural: '' }});
