b4428fbe1d0cf2a765d1ca38f18cafc92bf139bb
yshng <107883486+yshng@users.noreply.github.com>
2024-11-26 10:39:24 -0600

    less generic localstorage names
    Since these persist beyond session, concerned about clashing

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,36 @@ import "./App.css";
 import { WeatherContainer } from "./components/WeatherContainer";
 import { Button } from "./components/Button";
 import { UnitContext } from "./components/UnitContext";
-import { LocationInput } from "./components/LocationInput"; 
+import { LocationInput } from "./components/LocationInput";
 
 function App() {
-  const initialLocation = localStorage.getItem("location") || "chicago";
-  const initialUnitGroup = localStorage.getItem("unitGroup") || "us";
+  const initialLocation = localStorage.getItem("yesterweather_location") || "chicago";
+  const initialUnitGroup = localStorage.getItem("yesterweather_unitGroup") || "us";
 
   const [unitGroup, setUnitGroup] = useState(initialUnitGroup);
   const [location, setLocation] = useState(initialLocation);
 
   function toggleUnitGroup() {
-    if (unitGroup == "us") { 
-      setUnitGroup("metric")
-      localStorage.setItem("unitGroup", "metric")
-     } else {
+    if (unitGroup == "us") {
+      setUnitGroup("metric");
+      localStorage.setItem("yesterweather_unitGroup", "metric");
+    } else {
       setUnitGroup("us");
-      localStorage.setItem("unitGroup","us")
-     } 
+      localStorage.setItem("yesterweather_unitGroup", "us");
+    }
   }
 
-  const units = unitGroup == "us" ? {temp: "°F", depth: {unit: "inch", plural: "inches"}} : {temp: "°C", depth: {unit: "mm", plural: "mms"}};
+  const units =
+    unitGroup == "us"
+      ? { temp: "°F", depth: { unit: "inch", plural: "inches" } }
+      : { temp: "°C", depth: { unit: "mm", plural: "mms" } };
 
   return (
     <StrictMode>
       <UnitContext.Provider value={units}>
         <WeatherContainer unitGroup={unitGroup} location={location}>
           <h1>YesterWeather</h1>
-          <LocationInput value={location} onSubmit={(x) => setLocation(x)}/>
+          <LocationInput value={location} onSubmit={(x) => setLocation(x)} />
           <Button
             type="button"
             label={unitGroup}
