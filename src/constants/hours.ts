efbb0f972932d56d7b2cd82d269fa3981768c260
yshng <107883486+yshng@users.noreply.github.com>
2024-12-16 11:55:07 -0600

    rename file

--- src/constants/hours.ts
+++
@@ -1,9 +0,0 @@
-export function numberToHour(n: number) {
-  if (n == 0) return "12AM";
-  else if (n < 12) return n.toString() + "AM";
-  else if (n == 12) return "12PM";
-  else return (n - 12).toString() + "PM";
-}
-
-export const KEYS = [...Array(24).keys()];
-export const HOURS = KEYS.map((num) => numberToHour(num));
